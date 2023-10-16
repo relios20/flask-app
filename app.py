2956948a5407b024c8231d3dbe185951cf58a461
relios20 <75786938+relios20@users.noreply.github.com>
2023-10-16 19:55:49 +0000

    Adding HTML and CSS.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app =  Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-  return "Hello World"
+  return render_template('home.html')
+  
+
 
 if (__name__ == '__main__'):
   app.run(host='0.0.0.0', debug=True)
-  
